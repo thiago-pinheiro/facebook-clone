d304046141629c33f2903c75683bff73d7b727bb
Thiago Pinheiro <dev.thiagopinheiro@gmail.com>
2021-01-27 20:30:30 -0300

    Concluindo a AppBar

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import { ScrollView, StatusBar } from 'react-native'
+import styled from 'styled-components/native'
+import AppBar from './components/AppBar';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+
+const App = () => {
+  return(
+    <>
+    <StatusBar
+      backgroundColor="#ffffff"
+      barStyle="dark-content"
+    />
+
+    <Container>
+      <ScrollView>
+       <AppBar/>
+      </ScrollView>
+    </Container> 
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
